72062533d5c04e00c22f9ff10e2d8785443f916d
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-21 23:48:34 +0600

    Add files via upload

---
+++ Chef and His Fruit Stand/Chef and His Fruit Stand.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,b,t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        if((a/2)<b){
+            cout<<(a/2)<<endl;
+        }else{
+            cout<<b<<endl;
+
+        }
+
+    }
+
+
+
+
+
+    }
+
