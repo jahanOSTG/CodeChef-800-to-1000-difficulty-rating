54b6fe28add80f039d38efcef48144fd526c449a
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-21 23:37:00 +0600

    Add files via upload

---
+++ Paper Cutting/Paper Cutting.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,t,b;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        int c=a/b;
+        cout<<c*c<<endl;
+
+    }
+
+
+
+
+
+    }
+
