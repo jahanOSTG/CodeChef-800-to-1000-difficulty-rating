bb85d57a11fb0ccb3f7c034b6670589f4bf6a004
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-21 23:22:30 +0600

    Add files via upload

---
+++ Cars and Bikes/Cars and Bikes.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a;
+        if(a%4==0){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+
+
+
+
+
+    }
+
