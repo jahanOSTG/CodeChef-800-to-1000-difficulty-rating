3841c2503770762844bd5c6984ad67c723d68202
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-21 23:57:14 +0600

    Add files via upload

---
+++ Lazy Chef/Lazy Chef.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,a,b,c;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        int p=min((a*b),(a+c));
+        cout<<p<<endl;
+    }
+}
