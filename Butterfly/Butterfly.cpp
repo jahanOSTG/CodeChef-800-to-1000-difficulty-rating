a42a1993a126aa2b5e4a96f690ed1bfd5eaefd65
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-24 22:17:10 +0600

    Add files via upload

---
+++ Butterfly/Butterfly.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+	long long a,b,c;
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    cin>>a>>b>>c;
+	    long d=max({a,b,c});
+	    long sum=a+b+c;
+	    if(d<=sum-d){
+            cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+
+	}
+
+}
