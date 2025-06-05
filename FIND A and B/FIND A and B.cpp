d3648b55af19160c2f4889dd49db100ea4d16c7a
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-05 10:24:07 +0600

    Add files via upload

---
+++ FIND A and B/FIND A and B.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y,z;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y>>z;
+        if((x*y)%z == 0) cout<<(x*y)<<" "<<z<<endl;
+        else if((y*z) % x==0) cout<<(y*z)<<" "<<x<<endl;
+        else if((z*x) % y==0) cout<<(z*x)<<" "<<y<<endl;
+        else cout<<-1<<endl;
+
+    }
+}
